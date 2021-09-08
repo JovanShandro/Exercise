d60be154860e3fbc06c6e613957bc139f2e59e30
Jovan Shandro <jovan.shandro@identinet.io>
2021-09-08 16:08:13 +0200

    feat: show data from api in graph

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import DatePicker from "react-datepicker";
 import { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 
-const data = {
-  labels: ["1", "2", "3", "4", "5", "6"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgba(255, 99, 132, 0.2)",
-    },
-  ],
-};
-
 const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-        },
-      },
-    ],
+  plugins: {
+    legend: {
+      display: false,
+    },
   },
 };
 
@@ -42,6 +25,7 @@ const formatDate = (date) => {
 function App() {
   const [startDate, setStartDate] = useState(getDateOfTenDaysBefore());
   const [endDate, setEndDate] = useState(new Date());
+  const [graphData, setGraphData] = useState({});
 
   const fetchData = async () => {
     const resp = await fetch(
@@ -49,8 +33,19 @@ function App() {
         startDate
       )}&end=${formatDate(endDate)}&index=[USD]`
     );
-    const data = await resp.json();
-    console.log(data);
+    const { bpi } = await resp.json();
+
+    setGraphData({
+      labels: Object.keys(bpi),
+      datasets: [
+        {
+          data: Object.values(bpi),
+          fill: false,
+          backgroundColor: "blue",
+          borderColor: "blue",
+        },
+      ],
+    });
   };
 
   useEffect(() => {
@@ -75,7 +70,7 @@ function App() {
         </button>
       </div>
       <div className="graph">
-        <Line data={data} options={options} />
+        <Line data={graphData} options={options} />
       </div>
     </div>
   );
