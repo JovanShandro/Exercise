a8d6874ebcfebcdf68aeede664849bbf35142865
Jovan Shandro <jovan.shandro@identinet.io>
2021-09-08 15:41:51 +0200

    style: form and dummy line graph

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import DatePicker from "react-datepicker";
+import { useState } from "react";
+import { Line } from "react-chartjs-2";
+
+const data = {
+  labels: ["1", "2", "3", "4", "5", "6"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
-    <div>
-      App
+    <div className="container">
+      <div className="form">
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+        <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+        <button>Render</button>
+      </div>
+      <div className="graph">
+        <Line data={data} options={options} />
+      </div>
     </div>
   );
 }
